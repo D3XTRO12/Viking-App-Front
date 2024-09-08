486a85b4d8559c06c6a673a4b34de017de2053f1
Pablo, sometimes <mirazopablo@gmail.com>
2024-09-08 19:31:38 -0300

    #6 Modificacion App.tsx
    Modificacion completa para aceptar tabs publicas y privadas

--- App.tsx
+++ App.tsx
@@ -1,71 +1,41 @@
+// App.tsx
+import PublicTabs from './src/components/navigation/PublicTabs';
+import PrivateTabs from './src/components/navigation/PrivateTabs';
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Home from './src/screens/Home';
-import Login from './src/screens/Login';
-import WorkOrders from './src/screens/WorkOrder';
-import AddWorkOrder from './src/screens/AddWorkOrder';
-import AddDiagnosticPoint from './src/screens/AddDiagnosticPoint';
-import AddClients from './src/screens/AddClients';
-import AddDevice from './src/screens/AddDevice';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthProvider, useAuth } from './src/components/context/AuthContext';
 
-export type RootTabParamList = {
-  Home: undefined;
-  WorkOrders: undefined;
-  AddWorkOrder: undefined;
-  AddDiagnosticPoint: undefined;
-  AddClients: undefined;
-  AddDevice: undefined;
+export type RootStackParamList = {
+  PublicTabs: undefined;
+  PrivateTabs: undefined;
 };
 
-const Tab = createBottomTabNavigator<RootTabParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
-export default function App() {
+const Navigation = () => {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            let iconName: string;
-
-            switch (route.name) {
-              case 'Home':
-                iconName = 'home';
-                break;
-              case 'WorkOrders':
-                iconName = 'briefcase';
-                break;
-              case 'AddWorkOrder':
-                iconName = 'add-circle';
-                break;
-              case 'AddDiagnosticPoint':
-                iconName = 'medical';
-                break;
-              case 'AddClients':
-                iconName = 'person-add';
-                break;
-              case 'AddDevice':
-                iconName = 'hardware-chip';
-                break;
-              default:
-                iconName = 'ellipse';
-                break;
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="AddClients" component={AddClients} />
-        <Tab.Screen name="AddDevice" component={AddDevice} />
-        <Tab.Screen name="AddWorkOrder" component={AddWorkOrder} />
-        <Tab.Screen name="AddDiagnosticPoint" component={AddDiagnosticPoint} />
-        <Tab.Screen name="WorkOrders" component={WorkOrders} />
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {user == null ? (
+          <Stack.Screen name="PublicTabs" component={PublicTabs} />
+        ) : (
+          <Stack.Screen name="PrivateTabs" component={PrivateTabs} />
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Navigation />
+    </AuthProvider>
+  );
+}
+
+export default App;
