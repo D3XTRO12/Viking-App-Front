555979e2f7af6d27d203a69a1086daeb786e60ca
Pablo, sometimes <mirazopablo@gmail.com>
2024-09-08 19:25:18 -0300

    #7 Device Axios
    Modificacion del Archivo para que acepte las variables type, clientID y clientName a partir del json que recibe como respuesta de la api

--- src/components/axios/DeviceAxios.tsx
+++ src/components/axios/DeviceAxios.tsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { Alert } from 'react-native';
 import { DeviceInterface } from '../interfaces/DeviceInterface';
 
-
 const baseURL = 'http://172.28.205.8:8080';
+
 export const searchDevices = async (brand: string, setDevices: React.Dispatch<React.SetStateAction<DeviceInterface[]>>) => {
   try {
     const response = await axios.get(`${baseURL}/device/search`, {
@@ -12,8 +11,19 @@ export const searchDevices = async (brand: string, setDevices: React.Dispatch<Re
         brand: brand,
       },
     });
-    
-    setDevices(response.data); // Asigna los dispositivos encontrados al estado
+    console.log('Devices found:', response.data);
+
+    // Mapeamos la respuesta para adaptarla a DeviceInterface
+    const devices: DeviceInterface[] = response.data.map((device: any) => ({
+      id: device.id,
+      serialNumber: device.serialNumber,
+      brand: device.brand,
+      model: device.model,
+      type: device.type,
+      clientId: device.client.id,
+      clientName: device.client.name, // Añadir esta línea
+    }));
+    setDevices(devices); // Asigna los dispositivos encontrados al estado
   } catch (error) {
     console.error('Error fetching devices by brand:', error);
     // Manejo de errores según sea necesario
